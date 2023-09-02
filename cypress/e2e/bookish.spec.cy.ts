a456ef8669e70ab570aa9727fed501ceeb111631
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-09-02 15:06:48 -0500

    additional end to end tests, refactoring the app and confirming eveything still works

--- cypress/e2e/bookish.spec.cy.ts
+++ cypress/e2e/bookish.spec.cy.ts
@@ -1,6 +1,20 @@
+const url = 'http://localhost:3000/';
+
 describe('Bookish application', function () {
   it('Visits the bookish', function () {
-    cy.visit('http://localhost:3000/');
+    cy.visit(url);
     cy.get('h2[data-test="heading"]').contains('Bookish');
+  });
+
+  it('Shows a book list', () => {
+    cy.visit(url);
+    cy.get('div[data-test="book-list"]').should('exist');
+    // cy.get('div.book-item').should('have.length', 2);
+    cy.get('div.book-item').should((books) => {
+      expect(books).to.have.length(2);
+
+      const titles = [...books].map((x) => x.querySelector('h2').innerHTML);
+      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design']);
+    });
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
 import  Typography  from '@mui/material/Typography';
+import BookList from './BookList';
+
 
 function App() {
+
+  const books = [{ name: 'Refactoring'}, { name: 'Domain-driven design'}];
+
   return (
+    <>
     <Typography variant='h2' component='h2' data-test='heading'>
       Bookish
     </Typography>
+    <BookList books={books} />
+    </>
   );
 }
 
