b447315035e42cc6e607f057cc8214b769b32cbf
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-09-02 11:42:07 -0500

    make the first e2e test pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import  Typography  from '@mui/material/Typography';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Typography variant='h2' component='h2' data-test='heading'>
+      Bookish
+    </Typography>
   );
 }
 
